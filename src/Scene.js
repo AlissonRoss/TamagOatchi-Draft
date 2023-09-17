a4e4c966ce26377f11eb6c2452a5b0138d25f194
Alisson Ross <alisson@alisson.me>
2023-09-16 19:59:10 -0700

    Oatchi now bounces back and forth left-right

--- src/Scene.js
+++ src/Scene.js
@@ -5,6 +5,7 @@ export default function Scene() {
   const ref = useRef(); // reference to the DOM within reactJS
   const [ctx, setCtx] = useState(null); //Hook for context and setting the context
   const [x, setX] = useState(10); //x position Hook
+  const [oatchiDirection, setOatchiDirection] = useState(12);
   // Wrap the imageOatchi object initialization in useMemo to optimize performance
   const imageOatchi = useMemo(() => {
     const img = new Image();
@@ -25,27 +26,27 @@ export default function Scene() {
       const context = canvas.getContext('2d'); // drawing context so we can animate onto the canvas
       setCtx(context); // sets the context into a state Hook
     }
-
+    
     const animate = () => {
       draw();
       requestAnimationFrame(animate); // Call requestAnimationFrame recursively
     };
     animate();
       // Walk animation function
+    
   function walkAnimation() {
     console.log("Scene.js: x position in walking animation is :" + x);
-    if (x < window.innerWidth && x >= 0) {
-      // Move right if within the window boundaries
-      setX(prevX => prevX + 10);
-    } else if (x >= window.innerWidth) {
+    
+    if (x >= window.innerWidth - 400) {
       // Start moving left when it hits the window's right edge
-      setX(prevX => prevX - 10);
-    } else {
+      setOatchiDirection(-1);
+    } else if(x <= 0){
       // Reset to the right when it goes off the canvas
-      setX(window.innerWidth);
+      setOatchiDirection(1);
     }
+    setX(prevX => prevX + 10 * oatchiDirection); // Update the x position using setX
   }
-    const intervalId = setInterval(walkAnimation, 1000); // starts the walkAnimation in intervals of 1 second
+    const intervalId = setInterval(walkAnimation, 50); // starts the walkAnimation in intervals of 1 second
     console.log("Scene.js: setInterval was called");
     return () => clearInterval(intervalId); // Cleanup the interval on component unmount
   },[ctx, x, imageOatchi]); // Run the animation loop whenever the context or x position
